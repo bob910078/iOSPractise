543011eb2b9a2725acf746984bf8ca26a79fe9d7
Bob Chang <bob910078@gmail.com>
2019-01-01 16:03:06 +0800

    Initial Commit

---
+++ iOSPractise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSPractise
+//
+//  Created by Bob Chang on 1/1/19.
+//  Copyright © 2019 BobChang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
